8dbc0c7bcdcc8432f655b278b9454567fdfed7b1
Ryan DeBraal <ryandebraal@gmail.com>
2023-05-12 11:41:04 -0500

    Initial Commit

---
+++ Assets/Cell.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cell : MonoBehaviour, IDropHandler
+{
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+
+}
