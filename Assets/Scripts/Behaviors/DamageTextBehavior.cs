756430f019da48af5c34abac69cbd1362dab8749
Ryan DeBraal <ryandebraal@gmail.com>
2024-09-03 19:38:35 -0500

    tweaked damage text

--- Assets/Scripts/Behaviors/DamageTextBehavior.cs
+++ Assets/Scripts/Behaviors/DamageTextBehavior.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DamageTextBehavior : ExtendedMonoBehavior
 {
     public TextMeshPro textMesh;
-    public float floatSpeed;
+    public Vector3 speed;
+    [SerializeField] AnimationCurve riseCurve;
 
     #region Components
 
@@ -27,14 +29,16 @@ public class DamageTextBehavior : ExtendedMonoBehavior
     void Awake()
     {
         textMesh = GetComponent<TextMeshPro>();
-        floatSpeed = tileSize / 32;
+        speed = new Vector3(tileSize, tileSize / 32, 0);
     }
 
     public void Spawn(string text, Vector3 position)
     {
         textMesh.text = text;
-        var x = position.x + -(tileSize / 12 * Random.Percent) + (tileSize / 12 * Random.Percent);
-        var y = position.y + -(tileSize / 12 * Random.Percent) + (tileSize / 12 * Random.Percent);
+        //var x = position.x + -(tileSize / 12 * Random.Percent) + (tileSize / 12 * Random.Percent);
+        //var y = position.y + -(tileSize / 12 * Random.Percent) + (tileSize / 12 * Random.Percent);
+        var x = position.x;
+        var y = position.y + tileSize / 4;
         transform.position = new Vector3(x, y, 0);
         StartCoroutine(FadeOut());
     }
@@ -45,6 +49,9 @@ public class DamageTextBehavior : ExtendedMonoBehavior
         //Before:
         float alpha = 1;
         Color color = Colors.Solid.White;
+        Vector3 initialPosition = transform.position;
+        float timer = 0f;
+        float duration = 0.25f; // Time for one complete back-and-forth loop
 
         //During:
         while (textMesh.color.a > 0)
@@ -58,7 +65,18 @@ public class DamageTextBehavior : ExtendedMonoBehavior
                 textMesh.color = color;
             }
 
-            transform.position += new Vector3(0, floatSpeed, 0);
+            timer += Time.deltaTime;
+
+            // Calculate the normalized time (0 to 1) based on the duration
+            float normalizedTime = (timer % duration) / duration;
+
+            // Use the curve to determine the horizontal position
+            float curveValue = riseCurve.Evaluate(normalizedTime) * tileSize / 8;
+
+            var x = initialPosition.x + curveValue;
+            var y = position.y + speed.y;
+
+            transform.position = new Vector3(x, y, 0);
             yield return Wait.For(Interval.OneTick);
         }
 
